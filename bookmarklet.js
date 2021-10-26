ba2a5e0eb56554e987b47bec206f3e76bd8afb93
zane-programs <zstjohn22@windwardschool.org>
2021-10-26 11:00:32 -0700

    replace githack with jsdelivr

--- bookmarklet.js
+++ bookmarklet.js
@@ -10,7 +10,7 @@
     } else {
         const pickerScript = document.createElement("SCRIPT");
         pickerScript.id = "zaneFix";
-        pickerScript.src = "https://rawcdn.githack.com/zane-programs/pslearning-drive-picker-fix/e8cfd92bbdee232f7f1b99d9ef541dd9c998d897/fix.v0.2.1.min.js"
+        pickerScript.src = "https://cdn.jsdelivr.net/gh/zane-programs/pslearning-drive-picker-fix@e8cfd92bbdee232f7f1b99d9ef541dd9c998d897/fix.v0.2.1.min.js";
         pickerScript.onload = () => openPickerWindow();
         document.head.appendChild(pickerScript);
     }

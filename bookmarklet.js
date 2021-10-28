d5109f51df2f50af2adee81ed05877b1154c30ae
zane-programs <zstjohn22@windwardschool.org>
2021-10-27 17:55:56 -0700

    compile latest versions of fix & bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,7 +4,7 @@
     } else {
         const pickerScript = document.createElement("SCRIPT");
         pickerScript.id = "zaneFix";
-        pickerScript.src = "https://cdn.jsdelivr.net/gh/zane-programs/pslearning-drive-picker-fix@de409ccff0c50261c3231843bfff1168140dfdae/fix.v0.2.2.min.js";
+        pickerScript.src = "https://cdn.jsdelivr.net/gh/zane-programs/pslearning-drive-picker-fix@latest/fix.min.js";
         pickerScript.onload = () => openPickerWindow();
         document.head.appendChild(pickerScript);
     }
